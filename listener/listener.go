bd8ad8e8f673a4a2a4b3320808deb0b8505cbbfc
rumblefrog <xxlosttechxx@gmail.com>
2019-02-17 01:44:07 -0500

    listener: default timeout

--- listener/listener.go
+++ listener/listener.go
@@ -2,16 +2,21 @@ package listener
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-var Server *http.Server
+var (
+	Server         *http.Server
+	DefaultTimeout = time.Second * 3
+)
 
 func Start() {
 	Server = &http.Server{
-		Addr:    ":8080",
-		Handler: &VoteHandler{},
+		Addr:              ":8080",
+		Handler:           &VoteHandler{},
+		ReadHeaderTimeout: DefaultTimeout,
 	}
 
 	if err := Server.ListenAndServe(); err != http.ErrServerClosed {
